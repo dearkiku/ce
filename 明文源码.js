b2a867575dc272a63984c1a15e08badb1bc0aad3
Dear Kiku <50940366+dearkiku@users.noreply.github.com>
2024-12-23 15:21:11 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'bbe204b0-ccdb-44f3-8c59-241a364fa064';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
